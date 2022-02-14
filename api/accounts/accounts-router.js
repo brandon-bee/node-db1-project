10b62593f9c08659137eea5446cab302551968dc
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-02-14 14:09:11 -0800

    written first try of models

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -21,7 +21,10 @@ router.delete('/:id', (req, res, next) => {
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  })
 })
 
 module.exports = router;
